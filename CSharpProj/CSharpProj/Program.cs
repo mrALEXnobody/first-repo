24a3bb225f8f1859289bf86c9e6f1f18b07a9cb2
mrALEXnobody <scorp_predateur@mail.ru>
2019-02-17 16:19:54 +0300

    продолжил общение

--- CSharpProj/CSharpProj/Program.cs
+++ CSharpProj/CSharpProj/Program.cs
@@ -11,6 +11,7 @@ namespace CSharpProj
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("What's up");
         }
     }
 }
